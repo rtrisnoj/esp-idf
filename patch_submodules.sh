a4382c0d42730885c2332e8fd19a7c6c27a50b28
Deomid "rojer" Ryabkov <rojer@rojer.me>
2022-05-08 15:36:27 +0100

    Add lwip napt timestamp overflow fix

--- patch_submodules.sh
+++ patch_submodules.sh
@@ -3,4 +3,5 @@
 set -e -x
 
 patch -d components/lwip/lwip -p 1 < lwip.patch
+patch -d components/lwip/lwip -p 1 < lwip_napt_fix.patch
 patch -d components/bt/host/nimble/nimble -p 1 < nimble_reset_handle_on_restart.patch
